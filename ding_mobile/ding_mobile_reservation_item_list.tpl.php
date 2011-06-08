d7a15d6267b3182adbfacbd30932e8ef902cb3d8
Jesper Kristensen <cableman@linuxdev.dk>
2011-06-08 16:21:21 +0200

    Added theme function to help in creating a more mobile friendly reservation page

---
+++ ding_mobile/ding_mobile_reservation_item_list.tpl.php
@@ -0,0 +1,23 @@
+<h3><?php print $headline ?></h3>
+<ul<?php print drupal_attributes($attributes) ?> >
+  <?php foreach ($items as $item) { ?>
+    <li class="list-item">
+     <label>
+       <?php
+         print $item['checkbox'];
+         print $item['title'];
+       ?>
+     </label>
+
+     <ul class="information">
+       <?php foreach ($item['information'] as $info) { ?>
+        <li class="list-item">
+        <em class="label"><?php print $info['label'] ?></em>:
+        <span class="value"><?php $info['value'] ?></span>
+        </li>
+       <?php } ?>
+     </ul>
+    </li>
+  <?php } ?>
+</ul>
+
